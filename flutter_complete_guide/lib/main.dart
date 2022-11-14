8978f8f476366328c09b6749af2e41baeef038d9
piotr.jodko <piotr.jodko@gmail.com>
2022-11-14 21:55:50 +0100

    before adding new files

--- flutter_complete_guide/lib/main.dart
+++ flutter_complete_guide/lib/main.dart
@@ -1,34 +1,60 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
+// void main() {
+//   runApp(MyApp());
+// }
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _MyAppState();
+  }
 }
 
-class MyApp extends StatelessWidget {
-  void answerQuestion() {
-    print('answer chosen');
+class _MyAppState extends State<MyApp> {
+  var _questionIndex = 0;
+
+  void _answerQuestion() {
+    setState(() {
+      _questionIndex = _questionIndex + 1;
+    });
+    print(_questionIndex);
   }
 
   @override
   Widget build(BuildContext context) {
     var questions = [
-      'What color?',
-      'What\'s animal?',
+      'What\'s your favorite color?',
+      'What\'s your favorite animal?',
     ];
-
     return MaterialApp(
       home: Scaffold(
-          appBar: AppBar(
-            title: Text('first app'),
-          ),
-          body: Column(
-            children: <Widget>[
-              Text('The question'),
-              ElevatedButton(onPressed: this.answerQuestion, child: Text('Answer1')),
-              ElevatedButton(onPressed: answerQuestion, child: Text('Answer2')),
-              ElevatedButton(onPressed: answerQuestion, child: Text('Answer2'))
-            ],
-          )),
+        appBar: AppBar(
+          title: Text('My First App'),
+        ),
+        body: Column(
+          children: [
+            Text(questions[_questionIndex]),
+            ElevatedButton(
+              child: Text('Answer 1'),
+              onPressed: _answerQuestion,
+            ),
+            ElevatedButton(
+              child: Text('Answer 2'),
+              onPressed: () => print('Answer 2 chosen!'),
+            ),
+            ElevatedButton(
+              child: Text('Answer 3'),
+              onPressed: () {
+                // ...
+                print('Answer 3 chosen');
+              },
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
