fb57f41361a46065e962d491b1ddfc98e4bac519
piotr.jodko <piotr.jodko@gmail.com>
2023-01-19 22:43:53 +0100

    playing around with TransactionList

--- expanse_planner/lib/widgets/transaction_list.dart
+++ expanse_planner/lib/widgets/transaction_list.dart
@@ -12,7 +12,7 @@ class TransactionList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-        height: 500,
+        height: 600,
         child: userTransactions.isEmpty
             ? Column(
                 children: <Widget>[
@@ -50,41 +50,6 @@ class TransactionList extends StatelessWidget {
                       ),
                     ),
                   );
-                  // return Card(
-                  //     // color: Colors.orange,
-                  //     child: Row(
-                  //   children: <Widget>[
-                  //     Container(
-                  //         margin: const EdgeInsets.symmetric(vertical: 10, horizontal: 15),
-                  //         padding: const EdgeInsets.all(10),
-                  //         decoration: BoxDecoration(
-                  //             border: Border.all(
-                  //           width: 2,
-                  //           color: Theme.of(context).primaryColorDark,
-                  //         )),
-                  //         child: Text(
-                  //           style: TextStyle(
-                  //             fontWeight: FontWeight.bold,
-                  //             fontSize: 24,
-                  //             color: Theme.of(context).primaryColorDark,
-                  //           ),
-                  //           '\$${userTransactions[index].amount.toStringAsFixed(2)}',
-                  //         )),
-                  //     Column(
-                  //       crossAxisAlignment: CrossAxisAlignment.start,
-                  //       children: <Widget>[
-                  //         Text(
-                  //           style: Theme.of(context).textTheme.headline6,
-                  //           userTransactions[index].title,
-                  //         ),
-                  //         Text(
-                  //           // style: const TextStyle(color: Colors.grey),
-                  //           DateFormat('yyyy-MM-dd').format(userTransactions[index].date),
-                  //         ),
-                  //       ],
-                  //     )
-                  //   ],
-                  // ));
                 },
                 itemCount: userTransactions.length,
               ));
